eeb9b3fe8034e52940866c4aa1e43f5a6402b616
Tom Lawton <tom.lawton@cndu.it>
2024-02-17 16:38:41 +0000

    docs: gh pages build

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://talss89.github.io',
+	base: '/sprout',
 	integrations: [starlight({
 		title: 'Sprout',
 		customCss: [
