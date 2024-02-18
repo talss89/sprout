f46c1819fb728e0e116a83b4b3fa2ac3a5caa626
Tom Lawton <tom.lawton@cndu.it>
2024-02-18 22:58:50 +0000

    test: test fixture fact provider

---
+++ tests/common/fixture_facts.rs
@@ -0,0 +1,46 @@
+use anyhow::Result;
+use sprout::facts::ProjectFactProvider;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
+#[derive(Clone)]
+pub struct FixtureFacts {
+    pub path: PathBuf,
+    pub is_wordpress_installed: bool,
+    pub home_url: String,
+    pub hash: Option<String>,
+}
+
+impl FixtureFacts {}
+
+impl ProjectFactProvider for FixtureFacts {
+    fn is_wordpress_installed(&self) -> Result<bool> {
+        Ok(self.is_wordpress_installed)
+    }
+
+    fn get_home_url(&self) -> Result<String> {
+        Ok(self.home_url.to_owned())
+    }
+
+    fn get_uploads_dir(&self) -> Result<String> {
+        Ok(self.path.join("uploads/").to_string_lossy().to_string())
+    }
+
+    fn generate_unique_hash(&self) -> Result<Option<String>> {
+        Ok(self.hash.to_owned())
+    }
+
+    fn dump_database(&self, output_path: &Path, _home_url: &str) -> Result<()> {
+        fs::write(output_path, "-- This is a test fixture\nSHOW DATABASES;")?;
+        Ok(())
+    }
+    fn import_database(&self, _import_path: &Path) -> Result<()> {
+        Ok(())
+    }
+
+    fn postprocess_database(&self, _home_url: &str) -> Result<()> {
+        Ok(())
+    }
+}
