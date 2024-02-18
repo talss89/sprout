9f5de54790f5be6d3b98653d7bd7685d415f7538
Tom Lawton <tom.lawton@cndu.it>
2024-02-18 22:59:03 +0000

    test: test context utility

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -2,9 +2,38 @@ use anyhow::Result;
 use assert_cmd::Command;
 use predicates::prelude::*;
 use pretty_assertions::assert_eq;
-use std::fs;
+use sprout::engine::Engine;
+use std::{fs, path::PathBuf};
+use tempfile::TempDir;
+
+pub mod fixture_facts;
 
 pub type TestResult = Result<()>;
+
+#[derive(Debug)]
+pub struct TestContext {
+    pub sprout_home: TempDir,
+    pub stash_path: TempDir,
+    pub repo_path: TempDir,
+    pub engine: Engine,
+}
+
+impl TestContext {
+    pub fn new() -> Result<Self> {
+        let sprout_home = TempDir::new()?;
+        let stash_path = TempDir::new()?;
+        let repo_path = TempDir::new()?;
+
+        Ok(Self {
+            engine: Engine {
+                sprout_home: sprout_home.path().to_path_buf(),
+            },
+            sprout_home,
+            stash_path,
+            repo_path,
+        })
+    }
+}
 
 pub fn run(args: &[&str], expected_file: &str) -> Result<()> {
     let expected = fs::read_to_string(expected_file)?;
