d17969091666f920817109289a36b2b30fc3e5cb
Tom Lawton <tom.lawton@cndu.it>
2024-02-19 21:13:08 +0000

    minor: `ls` table tidy

--- src/cli/snapshot.rs
+++ src/cli/snapshot.rs
@@ -4,16 +4,22 @@ use std::io::Write;
 use tabwriter::TabWriter;
 
 use crate::snapshot::Snapshot;
-
+/// Generates a table showing all snapshots passed in
 pub fn project_table(snapshots: &Vec<Snapshot>) -> anyhow::Result<String> {
     let mut tw = TabWriter::new(vec![]).ansi(true);
 
     write!(
         &mut tw,
-        "{}",
-        "ID\tBranch\tFiles (New/Changed/Unchanged)\tSize\tDate / Time\n"
-            .dimmed()
-            .bold()
+        "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n",
+        "ID".dimmed().bold(),
+        "Branch".dimmed().bold(),
+        "Files".dimmed().bold(),
+        "New".dimmed().italic().cyan(),
+        "Change".dimmed().italic().cyan(),
+        "Same".dimmed().italic().cyan(),
+        "Size".dimmed().bold(),
+        "+Diff".dimmed().italic().cyan(),
+        "Date / Time".dimmed().bold()
     )?;
 
     for stash in snapshots {
@@ -22,22 +28,23 @@ pub fn project_table(snapshots: &Vec<Snapshot>) -> anyhow::Result<String> {
             &mut tw,
             "{}",
             format!(
-                "{}\t{}\t{} {}\t{}\t{}\n",
+                "{}\t{}\t{}\t{}\t{}\t{}\n",
                 stash.id.to_hex().to_string(),
                 stash.get_branch().unwrap_or("???".to_string()),
                 stash.get_total_files(),
                 match &stats {
                     Ok(stats) => format!(
-                        "({}/{}/{})",
+                        "{}\t{}\t{}",
                         stats.new.to_string().green(),
                         stats.changed.to_string().blue(),
                         stats.unmodified.to_string().yellow()
                     )
-                    .dimmed(),
+                    .dimmed()
+                    .italic(),
                     Err(_) => "(Err)".to_string().red(),
                 },
                 format!(
-                    "{} {}",
+                    "{}\t{}",
                     HumanBytes(stash.get_total_bytes()),
                     match &stats {
                         Ok(stats) => format!("(+{})", HumanBytes(stats.data_added)).dimmed(),
